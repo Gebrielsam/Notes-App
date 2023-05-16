49c5e986ad7c514955bc56a00ff1087ed8ae92ec
Onah Obinna Samuel <samgb042@gmail.com>
2023-05-16 13:46:29 +0100

    Updated version

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,23 @@
 module.exports = {
-  "output": {
-    "filename": "[name].pack.js"
+  output: {
+    filename: "[name].pack.js",
   },
-  "module": {
-    "rules": [
+  module: {
+    rules: [
       {
-        "use": {
-          "loader": "babel-loader",
-          "options": {
-            "presets": [
-              "babel-preset-env",
-              "babel-preset-react"
-            ]
-          }
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: [["@babel/plugin-proposal-class-properties"]],
+          },
         },
-        "exclude": /node_modules/,
-        "test": /\.js$/
-      }
-    ]
+        exclude: /node_modules/,
+        test: /\.js$/,
+      },
+    ],
   },
-  "entry": {
-    "index": "./index"
-  }
+  entry: {
+    index: "./index",
+  },
 };
